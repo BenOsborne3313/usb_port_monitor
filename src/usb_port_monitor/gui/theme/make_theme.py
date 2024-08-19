296619b5ef984af422a9d372460b1b12ededf295
BenOsborne3313 <beno3313@gmail.com>
2024-08-19 14:46:13 -0700

    feat(theme): working theme as json

--- src/usb_port_monitor/gui/theme/make_theme.py
+++ src/usb_port_monitor/gui/theme/make_theme.py
@@ -31,6 +31,18 @@ class Color:
     #make it json serializable
     def to_json(self):
         return {"r": self.r, "g": self.g, "b": self.b, "a": self.a}
+    
+
+#some functions to get the theme as a palette
+def load_theme_from_json(path_to_theme):
+    with open(path_to_theme, "r") as f:
+        theme = json.load(f)
+
+    theme_palette = QPalette()
+    for key, value in theme.items():
+        color = QColor(value["r"], value["g"], value["b"], value["a"])
+        theme_palette.setColor(getattr(QPalette, key), color)
+    return theme_palette
 
 #make an empty file in ./assets/ called theme.json
 if __name__ == "__main__":
@@ -52,7 +64,7 @@ if __name__ == "__main__":
     ]
 
     #now we set our colors
-    grey = QColor(53, 53, 53)
+    grey = QColor(16, 16, 16)
     white = QColor(255, 255, 255)
     visua_blue = QColor(0, 117, 183)
     black = QColor(0, 0, 0)
@@ -77,8 +89,9 @@ if __name__ == "__main__":
         "Highlight": visua_blue,
         "HighlightedText": black,
     }
-
-    with open(r"../assets/theme.json", "w") as f:
+    dir = os.path.abspath(__file__)
+    path_to_theme = os.path.join(os.path.dirname(dir), r"../assets/theme.json")
+    with open(path_to_theme, "w") as f:
         json.dump(theme_dict, f, indent=4)
     
     print("theme.json created")
